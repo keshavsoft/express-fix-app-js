7efc8484f550a9bd4020e897e5fc3eff5c4248dd
keshavsoft <nknnkumar@yahoo.com>
2026-06-14 23:14:24 +0530

    perfect

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import getLatestVersion from "./bin/core/getLatestVersion.js";
 
-const load = async ({ jsFilePath, inCheckLines, showLog }) => {
+const load = async ({ jsFilePath, inCheckLines, showLog, inStartEndPoint }) => {
     const v = getLatestVersion();
 
     const module = await import(`./bin/${v}/start.js`);
 
-    await module.default({ jsFilePath, inCheckLines, showLog });
+    await module.default({ jsFilePath, inCheckLines, showLog, inStartEndPoint });
 };
 
 export default load;
